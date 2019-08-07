d3c2a3c1c35fe22de0b99f79514533a6baf2afeb
tiana-horn <tiana.horn@gmail.com>
2019-08-06 21:47:50 -0400

    Fix Wsgi

--- flowerchild/wsgi.py
+++ flowerchild/wsgi.py
@@ -12,7 +12,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowerchild.settings')
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
-# application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
